50be190f14b8478731b40624bebb1575cfba2cea
Amy-X1 <anunobisplendour442@gmail.com>
2026-01-13 00:05:51 +0100

    updates

--- blog.js
+++ blog.js
@@ -1,28 +1,28 @@
+let list = document.querySelectorAll('.list');
+let cardtype = document.querySelectorAll('.bu');
 
-// let lis = document.querySelectorAll('.lis');
-// let bu = document.querySelectorAll('.bu');
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener('click', function() {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove('active');
+    }
+    this.classList.add('active');
 
-// for (let i = 0; i < list.length; i++) {
-//   list[i].addEventListener('click', function() {
-//     for (let j = 0; j < list.length; j++) {
-//       list[j].classList.remove('active');
-//     }
-//     this.classList.add('active');
+    let dataFilter = this.getAttribute('data-filter');
 
-//     // let dataFilter = this.getAttribute('data-filter');
+    for (let k = 0; k < cardtype.length; k++) 
+      {
+      cardtype[k].classList.remove('active');
+      cardtype[k].classList.add('hide');
 
-//     // for (let k = 0; k < cardtype.length; k++) 
-//     //   {
-//     //   cardtype[k].classList.remove('active');
-//     //   cardtype[k].classList.add('hide');
+      if (cardtype[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+        cardtype[k].classList.remove('hide');
+        cardtype[k].classList.add('active');
+      }
+    }
+  });
+}
 
-//     //   if (cardtype[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
-//     //     cardtype[k].classList.remove('hide');
-//     //     cardtype[k].classList.add('active');
-//     //   }
-//     // }
-//   });
-// }
 
 document.querySelector('.na').addEventListener('click', function(){
   document.querySelector('.ino1').classList.toggle('active');
